4767cce88b7f8ad0fe66d81a600cf6c29967bd4b
Raphael Barreto <raphael@gmail.com>
2021-04-17 17:10:36 -0300

    fix postgres conection

--- config/config.exs
+++ config/config.exs
@@ -5,4 +5,7 @@ config :rss_feed_discord_bot, ecto_repos: [RssFeedDiscordBot.Repo]
 config :nostrum,
   token: System.get_env("BOT_TOKEN")
 
-config :rss_feed_discord_bot, RssFeedDiscordBot.Repo, url: System.get_env("DATABASE_URL")
+config :rss_feed_discord_bot, RssFeedDiscordBot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20
